6987f841c6fa7bd14e533a82574abaf819ba46b1
Justin <sukiwhitex7@gmail.com>
2023-07-10 13:04:13 -0600

    Removed Some Unnecessary Code For Clarity

--- components/layout.js
+++ components/layout.js
@@ -13,10 +13,6 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle,
